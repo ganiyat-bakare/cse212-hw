7702bf330673ca7513c2595d24953bceabd6adb5
ganiyat-bakare <bak24006@byui.edu>
2025-01-28 13:25:27 +0100

    updated week 3 code added

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -161,6 +161,11 @@ public static class SetsAndMaps
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+        var result = new List<string>();
+
+        foreach (var feature in featureCollection.Features) {
+            result.Add($"{feature.Properties.Place} - Mag {feature.Properties.Mag}");
+        }
+        return result.ToArray();
     }
 }
